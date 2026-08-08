cd4ff350d37fcaf8f4f5eb42d9a5c054bd2ebd11
Ordean.Adrian <ordeanadrian725@gmail.com>
2026-08-08 14:30:54 +0300

    Pastebin - modifications

--- routes/paste.js
+++ routes/paste.js
@@ -1,35 +1,15 @@
 const express = require("express");
 const database = require("../helpers/db");
 const router = express.Router();
-const data = {
-    pastesList : []
-}
-let loaded = false;
-
-async function loadMainPage() {
-    const pastes = await database.getAllPastes();
-    for (let i = 0; i < pastes.length; ++i) {
-        data.pastesList.push(pastes[i]);
-    }
-}
 
 router.get("/main_page", async (req, res) => {
-    if (!loaded) {
-        await loadMainPage();
-        loaded = true;
-    }
-    res.render("fullList/index", data);
+    const data = await database.getAllPastes();
+    res.render("fullList/index",  { data });
 });
 
 router.post("/:id/edit", async (req, res) => {
     try {
         await database.updatePaste(req.params.id, req.body.content);
-        for (let i = 0; i < data.pastesList.length; ++i) {
-            if (data.pastesList[i].id === req.params.id) {
-                data.pastesList[i].content = req.body.content;
-                break;
-            }
-        }
         res.redirect("/paste/main_page");
     } catch(err) {
         console.error(err);
@@ -39,12 +19,6 @@ router.post("/:id/edit", async (req, res) => {
 router.post("/:id/delete", async (req, res) => {
     try {
         await database.deletePaste(req.params.id);
-        for (let i = 0; i < data.pastesList.length; ++i) {
-            if (data.pastesList[i].id === req.params.id) {
-                data.pastesList.splice(i, 1);
-                break;
-            }
-        }
         res.redirect("/paste/main_page");
 
     } catch(err) {
@@ -62,7 +36,7 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/content", async (req, res) => {
-    await data.pastesList.push(await database.addPasteIntoTheDataBase(req.body.content));
+    await database.addPasteIntoTheDataBase(req.body.content);
     res.redirect("/paste/main_page");
 });
 
